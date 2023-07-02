292016248f5a60957036f33a5867ce609cb860d8
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-07-03 02:06:26 +0300

    drh

--- lib/features/auth/login/page/login.dart
+++ lib/features/auth/login/page/login.dart
@@ -4,6 +4,7 @@ import 'package:live/app/core/utils/extensions.dart';
 import 'package:live/app/core/utils/images.dart';
 import 'package:live/app/core/utils/validation.dart';
 import 'package:flutter/material.dart';
+import 'package:live/components/custom_images.dart';
 import 'package:provider/provider.dart';
 import 'dart:ui' as ui;
 import '../../../../app/core/utils/text_styles.dart';
@@ -23,77 +24,97 @@ class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
-      width: context.width,
-      decoration: const BoxDecoration(
-        image: DecorationImage(image: AssetImage(Images.loginImage,),fit: BoxFit.fitWidth,)
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          BackdropFilter(
-            filter: ui.ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: Dimensions.PADDING_SIZE_EXTRA_LARGE.w),
-              padding: EdgeInsets.symmetric(horizontal: Dimensions.PADDING_SIZE_DEFAULT.w,vertical: 30.h),
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade500.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(25)),
-              child: Consumer<AuthProvider>(
-                builder: (_,provider,child) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                    Padding(
-                      padding:  EdgeInsets.fromLTRB(0,30.h,0,24.h),
-                      child: Text(
-                        getTranslated("login_header", context),
-                        style: AppTextStyles.w600.copyWith(
-                          fontSize: 28,
-                          color: ColorResources.WHITE_COLOR
-                        ),
-                      ),
-                    ),
-                      Form(
-                        key: _formKey,
-                        child: Column(children: [
-                          CustomTextFormField(
-                            controller: provider.mailTEC,
-                            hint: getTranslated("mail", context),
-                            inputType: TextInputType.emailAddress,
-                            valid: Validations.mail,
-                          ),
-
-                          CustomTextFormField(
-                            keyboardAction: TextInputAction.done,
-                            controller: provider.passwordTEC,
-                            hint: getTranslated("password", context),
-                            inputType: TextInputType.visiblePassword,
-                            valid: Validations.password,
-                          ),
-
-                          SizedBox(height: 20.h,),
-                          CustomButton(
-                              text: getTranslated("login", context),
-                              onTap: () {
-                                if (_formKey.currentState!.validate()) {
-                                  provider.logIn();
-                                }
-                              },
-                              isLoading: provider.isLoading),
-                        ],)
-                      ),
-                  ],);
-                }
-              ),
-            ),
+    return Scaffold(
+      body: Container(
+        width: context.width,
+        height: context.height,
+        padding: EdgeInsets.symmetric(
+            horizontal: Dimensions.PADDING_SIZE_EXTRA_LARGE.w),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(
+            Images.loginImage,
           ),
+          fit: BoxFit.fitWidth,
+        )),
+        child: Center(
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 75.h),
+                child: ClipRRect(
+                  clipBehavior: Clip.antiAlias,
+                  borderRadius: BorderRadius.circular(25),
+                  child: BackdropFilter(
+                    filter: ui.ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: Dimensions.PADDING_SIZE_DEFAULT.w,
+                          vertical: 30.h),
+                      decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(25)),
+                      child:
+                          Consumer<AuthProvider>(builder: (_, provider, child) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0, 30.h, 0, 24.h),
+                              child: Text(
+                                getTranslated("login_header", context),
+                                style: AppTextStyles.w600.copyWith(
+                                    fontSize: 28,
+                                    color: ColorResources.WHITE_COLOR),
+                              ),
+                            ),
+                            Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: [
+                                    CustomTextFormField(
+                                      controller: provider.mailTEC,
+                                      hint: getTranslated("mail", context),
+                                      inputType: TextInputType.emailAddress,
+                                      valid: Validations.mail,
 
-
-        ],
+                                    ),
+                                    CustomTextFormField(
+                                      keyboardAction: TextInputAction.done,
+                                      controller: provider.passwordTEC,
+                                      hint: getTranslated("password", context),
+                                      inputType: TextInputType.visiblePassword,
+                                      valid: Validations.password,
+                                    ),
+                                    SizedBox(
+                                      height: 20.h,
+                                    ),
+                                    CustomButton(
+                                        text: getTranslated("login", context),
+                                        onTap: () {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            provider.logIn();
+                                          }
+                                        },
+                                        isLoading: provider.isLoading),
+                                  ],
+                                )),
+                          ],
+                        );
+                      }),
+                    ),
+                  ),
+                ),
+              ),
+              customImageIcon(imageName: Images.logo, height: 140, width: 160),
+            ],
+          ),
+        ),
       ),
-    ),);
+    );
   }
 }
