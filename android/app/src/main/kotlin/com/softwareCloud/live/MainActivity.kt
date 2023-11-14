268abd4613068f71eca8d8ca98096cf70637fde9
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-11-14 02:00:58 +0200

    update auth

---
+++ android/app/src/main/kotlin/com/softwareCloud/live/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.softwareCloud.live
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+class MainActivity: FlutterActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        AppCenter.start(
+//            getApplication(), "43ad791e-9df0-4aa1-9c00-b27f84c2b9d5",
+//            Analytics::class.java, Crashes::class.java
+//        )
+//    }
+}
